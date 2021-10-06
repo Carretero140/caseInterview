6bf1560bbd6c745dd11997d6719e3c479b0f2d3e
Jose Carretero <Carretero140@gmail.com>
2021-10-06 16:57:25 -0500

    added code comment for function purpose

--- Pipeline.py
+++ Pipeline.py
@@ -6,6 +6,9 @@ import datetime
 
 class Pipeline:
     
+    """Function that will be used to extract data from json files 
+    and get them ready for transformation
+    """
     def extraction(self):
         for root, dirs, files in os.walk('requests'):
             for file in files:
