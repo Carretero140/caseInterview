5d161901e0176f779e378a60376643f26192a30b
Jose Carretero <Carretero140@gmail.com>
2021-10-06 16:53:02 -0500

    created extraction function

--- Pipeline.py
+++ Pipeline.py
@@ -6,3 +6,10 @@ import datetime
 
 class Pipeline:
     
+    def extraction(self):
+        for root, dirs, files in os.walk('requests'):
+            for file in files:
+                print(root,file)
+
+instance = Pipeline()
+instance.extraction()
