4c8575d854ff8b5c772f9f598628ae0f4f45b611
Jose Carretero <Carretero140@gmail.com>
2021-10-06 18:33:16 -0500

    added transformation function

--- Pipeline.py
+++ Pipeline.py
@@ -5,24 +5,35 @@ import datetime
 
 class Pipeline:
     
-    """Function that will be used to extract data from json files 
-    and get them ready for transformation
-    """
-    def extraction(self,year):
+    """Function that will be used to extract data from json files"""
+    def extraction(self,dir):
         dfs = []
         temp = pd.DataFrame()
-        for root,dirs,files in os.walk('requests'+year):
+        for root,dirs,files in os.walk(dir):
             if files:
                 for file in files:
                     data =pd.read_json(root+'\\'+file,lines = True)
                     dfs.append(data)
         temp = pd.concat(dfs,ignore_index=True)
-        #print(temp.head())
+        return temp
 
+    """Function that transforms the data"""
+    def transformation(self,df):
+        df["year"] = pd.DatetimeIndex(df["timestamp"]).year
+        df["month"] = pd.DatetimeIndex(df["timestamp"]).month
+        df["day"] = pd.DatetimeIndex(df["timestamp"]).day
+        df["hour"] = pd.DatetimeIndex(df["timestamp"]).hour
+        return df
+
+
+    """Function that will orchestrate the entire pipeline"""
     def orchestrate(self):
         directory = 'requests'
         for dir in next(os.walk(directory))[1]:
-            print(dir)
+            path = directory+'\\'+dir
+            df = self.extraction(path)
+            df = self.transformation(df)
+            print(df.head())
 
 
 
