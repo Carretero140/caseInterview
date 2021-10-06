72acf74332bc353c1422cb95a4ffa1bfe2ddbef2
Jose Carretero <Carretero140@gmail.com>
2021-10-06 18:14:54 -0500

    added extraction functionality to read json files

--- Pipeline.py
+++ Pipeline.py
@@ -3,16 +3,24 @@ import pandas as pd
 import datetime
 
 
-
 class Pipeline:
     
     """Function that will be used to extract data from json files 
     and get them ready for transformation
     """
-    def extraction(self):
-        for root, dirs, files in os.walk('requests'):
-            for file in files:
-                print(root,file)
+    def extraction(self,year):
+        dfs = []
+        temp = pd.DataFrame()
+        for root,dirs,files in os.walk('requests'+year):
+            if files:
+                for file in files:
+                    data =pd.read_json(root+'\\'+file,lines = True)
+                    dfs.append(data)
+        temp = pd.concat(dfs,ignore_index=True)
+        #print(temp.head())
 
+
+
+year = '\\2021'
 instance = Pipeline()
-instance.extraction()
+instance.extraction(year)
