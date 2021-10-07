27f2a01d2e2b3a348bcb1404c32f051ea22e1caf
Jose Carretero <Carretero140@gmail.com>
2021-10-06 19:19:15 -0500

    added load function

--- Pipeline.py
+++ Pipeline.py
@@ -1,6 +1,6 @@
 import os
 import pandas as pd
-import datetime
+from google.cloud import bigquery
 
 
 class Pipeline:
@@ -12,19 +12,26 @@ class Pipeline:
         for root,dirs,files in os.walk(dir):
             if files:
                 for file in files:
-                    data =pd.read_json(root+'\\'+file,lines = True)
+                    data = pd.read_json(root+'\\'+file,lines = True)
                     dfs.append(data)
         temp = pd.concat(dfs,ignore_index=True)
         return temp
 
     """Function that transforms the data"""
     def transformation(self,df):
+        df = df[["method","path","endpoint","view-args","status","user","timestamp","browser","browser-platform","browser-version"]]
         df["year"] = pd.DatetimeIndex(df["timestamp"]).year
         df["month"] = pd.DatetimeIndex(df["timestamp"]).month
         df["day"] = pd.DatetimeIndex(df["timestamp"]).day
         df["hour"] = pd.DatetimeIndex(df["timestamp"]).hour
         return df
 
+    def load(self):
+        credentials_path = r'C:\pythonProjects\interview\CaseInterview.privatekey.json'
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials_path
+
+        client = bigquery.Client()
+        table_id = 'caseinterview-328200.Log'
 
     """Function that will orchestrate the entire pipeline"""
     def orchestrate(self):
@@ -33,7 +40,8 @@ class Pipeline:
             path = directory+'\\'+dir
             df = self.extraction(path)
             df = self.transformation(df)
-            print(df.head())
+            self.load(df)
+            #print(df.head())
 
 
 
