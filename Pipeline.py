162f016211f7aab45ec529912846a5975cbc9901
Jose Carretero <Carretero140@gmail.com>
2021-10-06 20:48:31 -0500

    finished basic functionality

--- Pipeline.py
+++ Pipeline.py
@@ -1,4 +1,6 @@
 import os
+from google.cloud.bigquery import schema
+from google.cloud.bigquery.enums import WriteDisposition
 import pandas as pd
 from google.cloud import bigquery
 
@@ -7,6 +9,7 @@ class Pipeline:
     
     """Function that will be used to extract data from json files"""
     def extraction(self,dir):
+        print("entered extraction")
         dfs = []
         temp = pd.DataFrame()
         for root,dirs,files in os.walk(dir):
@@ -19,19 +22,61 @@ class Pipeline:
 
     """Function that transforms the data"""
     def transformation(self,df):
+        print("entered transform")
         df = df[["method","path","endpoint","view-args","status","user","timestamp","browser","browser-platform","browser-version"]]
+        df["method"] = df["method"].astype(str)
+        df["path"] = df["path"].astype(str)
+        df["endpoint"] = df["endpoint"].astype(str)
+        df["view-args"] = df["view-args"].astype(str)
+        df["user"] = df["user"].astype(str)
+        df["browser"] = df["browser"].astype(str)
+        df["browserplatform"] = df["browser-platform"].astype(str)
+        df["browser-version"] = df["browser-version"].astype(str)
         df["year"] = pd.DatetimeIndex(df["timestamp"]).year
         df["month"] = pd.DatetimeIndex(df["timestamp"]).month
         df["day"] = pd.DatetimeIndex(df["timestamp"]).day
         df["hour"] = pd.DatetimeIndex(df["timestamp"]).hour
+        df = df.rename(columns={"view-args": "view_args", "browser-platform": "browser_platform","browser-version":"browser_version"})
         return df
 
-    def load(self):
-        credentials_path = r'C:\pythonProjects\interview\CaseInterview.privatekey.json'
+    """Function that loads the dataframe into google bigquery"""
+    def load(self,df):
+        print("entered load")
+        credentials_path = 'C:\\pythonProjects\\interview\\CaseInterview.privatekey.json'
         os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials_path
 
         client = bigquery.Client()
-        table_id = 'caseinterview-328200.Log'
+        table_id = 'caseinterview-328200.Log.Records'
+
+        job_config = bigquery.LoadJobConfig(
+            schema = [
+                bigquery.SchemaField("method",bigquery.enums.SqlTypeNames.STRING),
+                bigquery.SchemaField("path",bigquery.enums.SqlTypeNames.STRING),
+                bigquery.SchemaField("endpoint",bigquery.enums.SqlTypeNames.STRING),
+                bigquery.SchemaField("view_args",bigquery.enums.SqlTypeNames.STRING),
+                bigquery.SchemaField("status",bigquery.enums.SqlTypeNames.INTEGER),
+                bigquery.SchemaField("user",bigquery.enums.SqlTypeNames.STRING),
+                bigquery.SchemaField("timestamp",bigquery.enums.SqlTypeNames.TIMESTAMP),
+                bigquery.SchemaField("browser",bigquery.enums.SqlTypeNames.STRING),
+                bigquery.SchemaField("browser_platform",bigquery.enums.SqlTypeNames.STRING),
+                bigquery.SchemaField("browser_version",bigquery.enums.SqlTypeNames.STRING),
+                bigquery.SchemaField("year",bigquery.enums.SqlTypeNames.INTEGER),
+                bigquery.SchemaField("month",bigquery.enums.SqlTypeNames.INTEGER),
+                bigquery.SchemaField("day",bigquery.enums.SqlTypeNames.INTEGER),
+                bigquery.SchemaField("hour",bigquery.enums.SqlTypeNames.INTEGER),
+            ],
+            write_disposition = "WRITE_TRUNCATE",
+        )
+        job = client.load_table_from_dataframe(df,table_id,job_config=job_config)
+        #job = client.insert_rows_from_dataframe(table = table_id,dataframe=df,chunk_size=500)
+        job.result()
+
+        table = client.get_table(table_id)
+        print(
+            "Loaded {} rows and {} columns to {}".format(
+                table.num_rows, len(table.schema),table_id
+            )
+        )
 
     """Function that will orchestrate the entire pipeline"""
     def orchestrate(self):
